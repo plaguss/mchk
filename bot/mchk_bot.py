d044339c6517260af3fbe545c8fc8d7936459215
agustin <agustin.piqueres@gmail.com>
2025-05-01 12:11:34 +0200

    Add bot folder

---
+++ bot/mchk_bot.py
@@ -0,0 +1,16 @@
+# /// script
+# requires-python = ">=3.12"
+# dependencies = [
+#     "discord-py",
+# ]
+# ///
+"""
+uv run mchk_bot.py
+"""
+
+def main() -> None:
+    print("Hello from mchk_bot.py!")
+
+
+if __name__ == "__main__":
+    main()
